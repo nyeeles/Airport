a633d464e74f3b42032a6f650973839bd323b04a
Nic Yeeles <n.yeeles@gmail.com>
2014-05-30 11:51:47 +0100

    Plane can fly

---
+++ lib/plane.rb
@@ -0,0 +1,14 @@
+class Plane
+
+	def initialize
+		@flying = false
+	end
+
+	def fly!
+		@flying = true
+	end
+
+	def flying?
+		@flying
+	end
+end
---
+++ spec/plane_spec.rb
@@ -0,0 +1,17 @@
+require 'plane'
+
+describe 'Plane' do
+	
+	let(:plane) { Plane.new }
+
+	it 'can not be flying' do
+		expect(plane).to_not be_flying
+	end
+
+	it 'can fly' do
+		plane = Plane.new
+		plane.fly!
+		expect(plane).to be_flying
+	end
+
+end
