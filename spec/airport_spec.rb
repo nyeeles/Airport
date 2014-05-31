e6ee6e02d4930f8c210edc5108723f1dc089c795
Nic Yeeles <n.yeeles@gmail.com>
2014-05-31 23:34:13 +0100

    Fixes all rspec errors for Airport

--- spec/airport_spec.rb
+++ spec/airport_spec.rb
@@ -1,4 +1,5 @@
 require 'airport'
+require 'plane'
 
 describe 'Airport' do
 
@@ -6,28 +7,28 @@ describe 'Airport' do
 	let(:plane) { Plane.new }
 	
 	it 'can be empty' do
-		expect(gatwick.terminal).to be_empty
+		expect(gatwick.storage).to be_empty
 	end
 
 	it 'can store a plane' do
-		gatwick.park(plane)
-		expect(gatwick.terminal).to_not be_empty	
+		gatwick.add_landing(plane)
+		expect(gatwick.storage).to_not be_empty	
 	end
 
 	it 'can count how many planes are parked in the terminal' do
-		gatwick.park(plane)
+		gatwick.add_landing(plane)
 		expect(gatwick.plane_count).to eq 1
 	end
 
-	it 'allows planes to dispatch the airport, one at a time' do
-		gatwick.park(plane)
-		gatwick.park(Plane.new)
-		gatwick.dispatch(plane)
+	it 'allows planes to depart the airport, one at a time' do
+		gatwick.add_landing(plane)
+		gatwick.add_landing(Plane.new)
+		gatwick.depart(plane)
 		expect(gatwick.plane_count).to eq 1
 	end
 
 	it 'changes planes status to \'not flying\'' do
 		expect(plane).to receive(:land!)
-		gatwick.park(plane)
+		gatwick.add_landing(plane)
 	end
 end
