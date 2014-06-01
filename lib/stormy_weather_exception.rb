9011d5dc22c3b133bf7df77fce743c898cba31b6
Nic Yeeles <n.yeeles@gmail.com>
2014-06-01 12:41:30 +0100

    Removes stormy_weather_exception files

--- lib/stormy_weather_exception.rb
+++
@@ -1,3 +0,0 @@
-class StormyWeatherException < Exception 
-	
-end
--- spec/stormy_weather_exception_spec.rb
+++
@@ -1,8 +0,0 @@
-require 'stormy_weather_exception'
-
-describe 'StormyWeatherException' do
-	
-	it 'raies an error when the weather is bad' do
-		double = storm, error: :bad
-	end
-end
